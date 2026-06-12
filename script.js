f9df8bc0e8b7a89be79c33ace972913605b5c9ee
karenmarciano-sketch <karen.marciano@escola.pr.gov.br>
2026-06-12 18:55:43 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+let plantas = 0;
+
+const tipos = [
+  "🌱​",
+  "🌾",
+  "🌻",
+  "🌿",
+  "🍀"
+  "🏵️",
+  "🌸"
+];
+
+function plantar(){
+
+  plantas++;
+
+  document.getElementById("contador").innerText =
+    "Plantas cultivadas: " + plantas;
+
+  let planta =
+    tipos[Math.floor(Math.random() * tipos.length)];
+
+  document.getElementById("jardim").innerHTML += planta;
+
+}
